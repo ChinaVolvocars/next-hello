54d68f5134b43769881b1840efcfd6ac297bd5a1
Atlantis <atlantisspeed@gmail.com>
2023-07-22 23:19:07 +0800

    Error

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import {Metadata} from 'next'
+import {ErrorBoundary} from "next/dist/client/components/error-boundary";
+import Error from "@/app/dashboard/error";
+import {RootLayoutError} from "next/dist/client/components/react-dev-overlay/internal/container/RootLayoutError";
 
 export default function DashboardLayout({children}: { children: React.ReactNode }) {
   return (
